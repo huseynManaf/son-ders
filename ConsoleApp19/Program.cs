3cc802ea378dcff9a0164a09fe01332e5d4b4926
huseynManaf <huseynim@code.edu.az>
2022-12-22 04:22:19 -0800

    init

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            pervane master
+                /
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
